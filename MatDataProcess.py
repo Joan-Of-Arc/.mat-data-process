49e6b31e675a3b3fa358baa7eeb1db0fac325c74
Qliphoth <1841362281@qq.com>
2022-11-18 17:36:00 +0800

    种类信息也一块返回

--- MatDataProcess.py
+++ MatDataProcess.py
@@ -3,7 +3,12 @@ import scipy.io as scio
 class MatDataProcess:
     """ 处理mat数据, 返回附带测量角度的信息 """
     def __init__(self, data, data_key, label_key) -> None:
-        """ 初始化传入一个mat对象(文件路径), 先对all_label进行划分 """
+        """ 
+        初始化传入一个mat对象(文件路径), 先对all_label进行划分 
+        data: mat文件路径
+        data_key: mat 文件中数据的键, 如 all_data
+        label_key: mat 文件中标签的键, 如 all_label  
+        """
         self.data = scio.loadmat(data)
         self.data_key = data_key
         self.label_key = label_key
@@ -33,35 +38,34 @@ class MatDataProcess:
         return self
 
     def __next__(self):
-        """ 定义迭代时的方法 """
+        """ 定义迭代时的next方法 """
         cnt = self.cnt  # 目前读到的行数
         data_data = self.data[self.data_key]  # 获取到数据
+
         if cnt >= self.len: 
             raise StopIteration  # 迭代终止
         
         # 以下为角度计算
-        cur = 0
         for i, n in enumerate(self.devide):
-            if cnt > n: 
-                cur = n
-                continue
-        if cur: cnt -= cur + 1
+            if cnt <= n: break
+        cnt -= 0 if i == 0 else self.devide[i - 1] + 1
         # 角度范围为 0 - 350, 以10°为间隔将同一类的数据进行连续划分
         angle = int((((cnt / self.numbers[i]) * 360) // 10) * 10)
         cur = data_data[self.cnt]
         self.cnt += 1
-        # 将角度与原数据打包返回
-        return (cur, angle)
+        return (cur, self.kind[i], angle)  # 将原数据, 种类, 角度 打包返回
 
 if __name__ == "__main__":
-    data = "a2h1.mat"
+    data = "./datas/a2h1.mat"
     M = MatDataProcess(data, "all_data", "all_label")
+    # data = "testdata.mat"
+    # M = MatDataProcess(data, "b", "a")
     print(M.devide, M.kind, M.numbers, len(M.data[M.label_key]))
-    cnt = 0
+    # cnt = 0
     for m in M: 
-        if cnt >= 80: break
-        print(m[1])
-        cnt += 1
+        # if cnt >= 80: break
+        print(m[1:])
+        # cnt += 1
     
 
 
